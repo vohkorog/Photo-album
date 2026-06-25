2b233bb847ed259297591409e4c0efd60add3714
user <mywowa@yandex.ru>
2026-06-25 16:27:12 +0300

    изменение схем для атворизации пользователя

--- serves_4/src/auth/schemas.py
+++ serves_4/src/auth/schemas.py
@@ -6,15 +6,6 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
 
-class UserScheme():
-    id: int
-    email: EmailStr
-    username: str
-    hashed_password: str
-    is_active: bool = True
-    created_at: datetime = datetime.now()
-    refresh_token: str | None = None
-
 class UserCreateScheme(BaseModel):
     email: EmailStr
     username: str
@@ -23,15 +14,3 @@ class UserCreateScheme(BaseModel):
 class UserLoginScheme(BaseModel):
     username: str
     password: str
-
-class UserInDBScheme(UserScheme):
-    pass
-
-class TokenScheme(BaseModel):
-    access_token: str
-    refresh_token: str
-    token_type: str = "bearer"
-
-class TokenDataScheme(BaseModel):
-    username: str
-    token_type: str  # "access" or "refresh"
